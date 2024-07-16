9fdd0d4c4edf006a8e6bf22fafabd54fc464ba0e
loreloc <lorenzoloconte@outlook.it>
2024-07-16 11:55:48 +0100

    unsqueeze channel dimension

--- src/scripts/engine.py
+++ src/scripts/engine.py
@@ -419,6 +419,8 @@ class Engine:
             for batch_idx, batch in enumerate(train_dataloader):
                 if isinstance(batch, (tuple, list)):
                     batch = batch[0]
+                if isinstance(self.model, PC):
+                    batch = batch.unsqueeze(dim=1)
                 batch = batch.to(self._device)
                 if isinstance(self.model, PC):
                     lls = self.model.log_likelihood(batch)
