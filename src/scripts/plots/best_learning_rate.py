1a313e1f0a6b3f1eec06db8d71c573d2a20ceb57
loreloc <lorenzoloconte@outlook.it>
2024-08-04 12:13:23 +0100

    removed lowest learning rate from sos-npcs.json

--- src/scripts/plots/best_learning_rate.py
+++ src/scripts/plots/best_learning_rate.py
@@ -10,6 +10,8 @@ parser = argparse.ArgumentParser(
 parser.add_argument("tboard_path", type=str, help="The Tensorboard runs path")
 parser.add_argument("filename", type=str, help="The output filename")
 parser.add_argument("--metric", default="avg_ll", help="The metric considered")
+parser.add_argument('--exp-sos', action='store_true', default=False,
+                    help="Whether to group ExpSOS experiment results by the number of units in the monotonic circuits")
 
 
 if __name__ == '__main__':
@@ -20,7 +22,9 @@ if __name__ == '__main__':
     metrics = [train_metric, valid_metric, test_metric]
     df = retrieve_tboard_runs(args.tboard_path, metrics)
     #df = df[df['seed'] == 123]
-    group_by_cols = ['dataset', 'model', 'exp_alias', 'num_components', 'mono_num_units', 'mono_num_input_units']
+    group_by_cols = ['dataset', 'model', 'exp_alias', 'num_components']
+    if args.exp_sos:
+        group_by_cols.extend(['mono_num_units', 'mono_num_input_units'])
     cols_to_keep = group_by_cols + metrics + ['learning_rate', 'num_sum_params', 'num_params']
     df = df.drop(df.columns.difference(cols_to_keep), axis=1)
     df = df.sort_values(valid_metric, ascending=False)
