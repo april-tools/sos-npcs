4b0abcd5f3a17d020a686615e86476879eb26c48
loreloc <lorenzoloconte@outlook.it>
2024-08-10 17:51:16 +0100

    minor

--- src/scripts/plots/sample_images.sh
+++ src/scripts/plots/sample_images.sh
@@ -7,9 +7,6 @@ DEVICE=${DEVICE:-cuda}
 for dataset in MNIST
 do
   python -m "$PYSCRIPT" "$CHECKP_PATH" "$dataset" SOS \
-    "SOS_RGqt_R1_K128_KI128_OAdam_LR0.01_BS256" "1723292540.023343" --exp-alias "real" \
-    --num-units 128 --device $DEVICE
-  python -m "$PYSCRIPT" "$CHECKP_PATH" "$dataset" SOS \
     "SOS-C_RGqt_R1_K64_KI64_OAdam_LR0.01_BS256" "1723292540.283073" --exp-alias "complex" \
-    --num-units 64 --device $DEVICE
+    --complex --num-units 64 --device $DEVICE
 done
