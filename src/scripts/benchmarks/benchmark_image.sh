34a93bae41bfef29f293a8bfc7a1bdfec9a99652
loreloc <lorenzoloconte@outlook.it>
2024-08-17 16:23:05 +0100

    minor

--- src/scripts/benchmarks/benchmark_image.sh
+++ src/scripts/benchmarks/benchmark_image.sh
@@ -4,7 +4,7 @@ PYSCRIPT="scripts.benchmarks.benchmark"
 TBOARD_PATH="${TBOARD_PATH:-tboard-runs/image-data-complete}"
 DATA_PATH="${DATA_PATH:-datasets}"
 
-BENCHMARK_FLAGS="--backprop --num-iterations 50 --burnin-iterations 5 --batch-size 256 --data-path $DATA_PATH"
+BENCHMARK_FLAGS="--backprop --num-iterations 100 --burnin-iterations 10 --batch-size 256 --data-path $DATA_PATH"
 
 
 python -m "$PYSCRIPT" "$TBOARD_PATH" MNIST MPC $BENCHMARK_FLAGS --num-units "16 32 64 128 256 512"
@@ -12,7 +12,7 @@ python -m "$PYSCRIPT" "$TBOARD_PATH" MNIST SOS $BENCHMARK_FLAGS --num-units "16 
 python -m "$PYSCRIPT" "$TBOARD_PATH" MNIST SOS $BENCHMARK_FLAGS --complex --num-units "8 16 32 64 128 256"
 python -m "$PYSCRIPT" "$TBOARD_PATH" MNIST ExpSOS $BENCHMARK_FLAGS --complex --mono-num-units 8 --num-units "8 16 32 64 128 256"
 
-#python -m "$PYSCRIPT" "$TBOARD_PATH" CelebA MPC $BENCHMARK_FLAGS --num-units "16 32 64 128 256"
-#python -m "$PYSCRIPT" "$TBOARD_PATH" CelebA SOS $BENCHMARK_FLAGS --num-units "16 32 64 128"
-#python -m "$PYSCRIPT" "$TBOARD_PATH" CelebA SOS $BENCHMARK_FLAGS --complex --num-units "8 16 32 64 128"
-#python -m "$PYSCRIPT" "$TBOARD_PATH" CelebA ExpSOS $BENCHMARK_FLAGS --complex --mono-num-units 8 --num-units "4 8 16 32"
+python -m "$PYSCRIPT" "$TBOARD_PATH" CelebA MPC $BENCHMARK_FLAGS --num-units "16 32 64 128 256"
+python -m "$PYSCRIPT" "$TBOARD_PATH" CelebA SOS $BENCHMARK_FLAGS --num-units "16 32 64 128"
+python -m "$PYSCRIPT" "$TBOARD_PATH" CelebA SOS $BENCHMARK_FLAGS --complex --num-units "8 16 32 64 128"
+python -m "$PYSCRIPT" "$TBOARD_PATH" CelebA ExpSOS $BENCHMARK_FLAGS --complex --mono-num-units 8 --num-units "4 8 16 32"
