69f46b96f9bc72ef7a3abf6292d626a1faaa1ab7
loreloc <lorenzoloconte@outlook.it>
2024-12-14 11:06:02 +0000

    removed opt and moved them to cirkit

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,10 +1,5 @@
-from typing import cast
-
-from cirkit.backend.torch.compiler import TorchCompiler
-from cirkit.backend.torch.optimization.registry import ParameterOptRegistry
 from cirkit.pipeline import PipelineContext
 from initializers import compile_exp_uniform_initializer
-from optimizations import OuterProductReduceSumPattern, apply_prod_sum_einsum
 
 
 def setup_pipeline_context(
@@ -18,11 +13,4 @@ def setup_pipeline_context(
         backend=backend, semiring=semiring, fold=fold, optimize=optimize
     )
     ctx.add_initializer_compilation_rule(compile_exp_uniform_initializer)
-    compiler = cast(TorchCompiler, ctx._compiler)
-    parameter_opt_registry = cast(
-        ParameterOptRegistry, compiler._optimization_registry["parameter"]
-    )
-    parameter_opt_registry.add_rule(
-        apply_prod_sum_einsum, signature=OuterProductReduceSumPattern
-    )
     return ctx
